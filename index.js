abbef6bd351b5a53aa8f4423268de2960a7c1cd2
Mohit Gawande <mohitgawande65@gmail.com>
2022-08-21 15:07:49 +0530

    added mongoose lib to connect with mongodb database

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+require('dotenv').config()
 const express=require('express')
 const app= express()
 const port=3000;
-const employees = require('./Router')
+const employeesRouter = require('./Router')
+const mongoose=require('mongoose')
+const db=mongoose.connection
+const url=process.env.DATABASE_URL
+mongoose.connect(url)
+
+db.on('error', console.log)
+
+db.once('open', () => {
+    console.log('connection to mongodb started successfully')
+})
 
 app.use(express.json())
 
-app.use('/employees',employees)
+app.use('/employees',employeesRouter)
 
 app.listen(port,()=>{
     console.log(`express app is listening on https://localhost:${port}/employees`)
